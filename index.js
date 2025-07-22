701cab45d1f31254f1dc8024982fe3908f53db5a
kabir26ideas <kabir.seth@26ideas.com>
2025-07-22 12:36:07 +0530

    adding new backend

--- index.js
+++ index.js
@@ -6,29 +6,87 @@ dotenv.config();
 
 const app = express();
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const assistant_id= process.env.ASSISTANT_ID;
+
+console.log(process.env.OPENAI_API_KEY);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/chat', async (req, res) => {
-  const userMessage = req.body.message;
 
+app.post('/chat', async (req, res) => {
+  console.log("test /chat");
+  const userMessage = req.body.message;
+  const threadid= req.body.user.threadId;
+  const questionid= req.body.question.questionId;
+  
+console.log(threadid);
+console.log(questionid);
   if (!userMessage) {
     return res.status(400).json({ reply: "Message is required." });
   }
 
-  try {
-    const completion = await openai.chat.completions.create({
-      model: "26ideas_faq_chatbot",
-      messages: [{ role: "user", content: userMessage }],
-    });
+  if(!threadid){
+    return res.status(400).json({ reply: "threadid is required" });
+  //make a method to update thread id if no threadid is found?
+  }
 
-    const reply = completion.choices[0].message.content.trim();
-    res.json({ reply });
+  if(!questionid){
+    return res.status(400).json({ reply: "questionid is required" });
+  }
+
+  try {
+
+await openai.beta.threads.messages.create(threadid, {
+  role: "user",
+  content: `QuestionID: ${questionid}\nAnswer: ${userMessage}`,
+
+
+});
+
+const run = await openai.beta.threads.runs.create(threadid,{
+  assistant_id: assistant_id,
+});
+
+let runstatus; // run status for response 
+let attempts = 0;
+const maxAttempts = 60;// max attempts or timeout seconds
+do{
+  runstatus= await openai.beta.threads.runs.retrieve(threadid,run.id);
+   console.log(runstatus);
+  if(runstatus.status === "completed") break;
+  await new Promise((resolve) => setTimeout(resolve,1000));
+  attempts++;
+} while ((runstatus.status === "queued" ||
+   runstatus.status === "in_progress") && 
+   attempts < maxAttempts);
+
+
+//fetching response
+
+const messages = await openai.beta.threads.messages.list(threadid);
+const lastMessage = messages.data.find((msg)=> msg.role === "assistant");
+
+res.json({
+  reply: lastMessage?.content[0]?.text?.value || "No reply try again",
+ 
+});
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ reply: "Failed to get response from OpenAI." });
   }
+
+
+
+
+
+
+
+  
+
+
+
 });
 
 const PORT = process.env.PORT || 3000;
