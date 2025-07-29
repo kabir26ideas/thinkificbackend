1bcf4d6ccfb0421fbc8c3df425bd6c12fecf6bf0
kabir26ideas <kabir.seth@26ideas.com>
2025-07-29 15:20:51 +0530

    adding compilor route

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const YFP_NARATIVE_Q12ER= process.env.YFP_NARATIVE_Q12ER;
 const YFP_NARATIVE_Q1OP= process.env.YFP_NARATIVE_Q1OP;
 const YFP_NARATIVE_Q2OP= process.env.YFP_NARATIVE_Q2OP;
 const YFP_NARATIVE_Q3OP= process.env.YFP_NARATIVE_Q3OP;
+const YFP_NARATIVE_COMPILOR=process.env.YFP_NARATIVE_COMPILOR;
 
 
 
@@ -1817,6 +1818,82 @@ await openai.beta.threads.messages.create(threadid, {
 
 const run = await openai.beta.threads.runs.create(threadid,{
   assistant_id: YFP_NARATIVE_Q3OP,
+});
+
+let runstatus; // run status for response 
+let attempts = 0;
+const maxAttempts = 60;// max attempts or timeout seconds
+do{
+  runstatus= await openai.beta.threads.runs.retrieve(threadid,run.id);
+   console.log(runstatus);
+  if(runstatus.status === "completed") break;
+  await new Promise((resolve) => setTimeout(resolve,1000));
+  attempts++;
+} while ((runstatus.status === "queued" ||
+   runstatus.status === "in_progress") && 
+   attempts < maxAttempts);
+
+
+//fetching response
+
+const messages = await openai.beta.threads.messages.list(threadid);
+const lastMessage = messages.data.find((msg)=> msg.role === "assistant");
+
+res.json({
+  reply: lastMessage?.content[0]?.text?.value || "No reply try again",
+ 
+});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ reply: "Failed to get response from OpenAI." });
+  }
+
+
+
+
+
+
+
+  
+
+
+
+});
+
+
+app.post('/chatcompile', async (req, res) => {
+  console.log("test /chat");
+  const userMessage = req.body.message;
+  const threadid= req.body.user.threadId;
+  // const questionid= 1;
+  
+console.log(threadid);
+// console.log(questionid);
+  if (!userMessage) {
+    return res.status(400).json({ reply: "Message is required." });
+  }
+
+  if(!threadid){
+    return res.status(400).json({ reply: "threadid is required" });
+  //make a method to update thread id if no threadid is found?
+  }
+
+  // if(!questionid){
+  //   return res.status(400).json({ reply: "questionid is required" });
+  // }
+
+  try {
+
+await openai.beta.threads.messages.create(threadid, {
+  role: "user",
+  content: `Answer: ${userMessage}`,
+
+
+});
+
+const run = await openai.beta.threads.runs.create(threadid,{
+  assistant_id: YFP_NARATIVE_COMPILOR,
 });
 
 let runstatus; // run status for response 
