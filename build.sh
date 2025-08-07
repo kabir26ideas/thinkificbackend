aa2c85d1ae1fa39155abdb62121406d94a62169d
kabir26ideas <kabir.seth@26ideas.com>
2025-08-07 14:51:44 +0530

    adding build script for render

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Install Chromium for Puppeteer
 echo "Installing Chromium for Puppeteer"
 npx puppeteer browsers install chrome
+
+# Install all project dependencies
+echo "Running npm install"
+npm install
