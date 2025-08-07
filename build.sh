313ddb59c46f5acedd869e47bd98200f5979ae21
kabir26ideas <kabir.seth@26ideas.com>
2025-08-07 17:45:41 +0530

    render

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
+# #!/usr/bin/env bash
+
+# # Install Chromium for Puppeteer
+# echo "Installing Chromium for Puppeteer"
+# npx puppeteer browsers install chrome
+
+# # Install all project dependencies
+# echo "Running npm install"
+# npm install
+
+
 #!/usr/bin/env bash
+# exit on errorset -o errexit
 
-# Install Chromium for Puppeteer
-echo "Installing Chromium for Puppeteer"
-npx puppeteer browsers install chrome
-
-# Install all project dependencies
-echo "Running npm install"
 npm install
+# npm run build # uncomment if required
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+  echo "...Copying Puppeteer Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else 
+  echo "...Storing Puppeteer Cache in Build Cache" 
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
